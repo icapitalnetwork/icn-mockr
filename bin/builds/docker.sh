2a2c8f571aae9693a08932aae22a38c6c14a8b82
Freddy Kennedy <89410753+fkennedy1@users.noreply.github.com>
2023-07-27 12:44:37 -0400

    Fixing docker entrypoint script

--- bin/builds/docker.sh
+++ bin/builds/docker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "${*}" == "bundle exec rails server -b 0.0.0.0" ]; then
-  bundle exec rails db:create db:migrate
+  bundle exec rails db:migrate
+  bundle exec rails assets:precompile
 fi
 
 exec "$@"
